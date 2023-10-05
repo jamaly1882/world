0e9cea0068324def34eab402cd6b89880835eb58
aljamali <jamly2013@outlook.com>
2023-10-05 17:08:44 +0300

    db_up

--- world/settings.py
+++ world/settings.py
@@ -78,22 +78,22 @@ WSGI_APPLICATION = 'world.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-       # 'NAME': BASE_DIR / 'db.sqlite3',
-   # }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'markt',
-        'USER': 'root',
-        'PASSWORD': 'Egypt-+20!!',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#DATABASES = {
+    #'default': {
+       # 'ENGINE': 'django.db.backends.mysql',
+       # 'NAME': 'markt',
+       # 'USER': 'root',
+       # 'PASSWORD': 'Egypt-+20!!',
+       # 'HOST': 'localhost',
+       # 'PORT': '3306',
+    #}
+#}
 
 
 
